e004fd8d5bb7d1d4f8b510e13035c68c053c90b8
T-c-day <taylor091@revature.net>
2022-03-31 12:14:04 -0700

    updated project

---
+++ src/app/components/doctor/doctor.component.ts
@@ -0,0 +1,61 @@
+import { DoctorService } from '../../services/doctor.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Doctor } from 'src/app/models/doctor';
+import { Router } from '@angular/router';
+import { SearchParametersService } from 'src/app/services/search-parameters.service';
+
+@Component({
+  selector: 'app-doctor',
+  templateUrl: './doctor.component.html',
+  styleUrls: ['./doctor.component.css']
+})
+export class DoctorComponent implements OnInit {
+  
+  doctors: Doctor[] = [];
+  errorMessage !: string
+
+  @Input() searchSpec!:string;
+  @Input() searchPro!:string;
+
+
+  @Input() searchParam:string[] = [];
+
+  displayFlag!:boolean;
+
+  constructor(public DoctorService: DoctorService, public searchService:SearchParametersService, public router: Router ) {
+    // this.searchSpec = searchService.getSearchSpecialty();
+    // this.searchPro = searchService.getSearchProvider();
+    // this.displayFlag = searchService.getDisplayFlag();
+   }
+
+  ngOnInit(): void {
+    // this.searchService.specSource.subscribe((data) => {
+    //   this.searchSpec = data; })
+
+    //   this.searchService.proSource.subscribe((data) => {
+    //     this.searchPro = data;})
+
+    //this.displayDoctorInfo();
+    this.searchPro = "Anthem Blue Cross Blue Shield";
+    this.displayBySearch(this.searchSpec, this.searchPro); // this.searchSpec
+  }
+
+  displayDoctorInfo(){
+    console.log();
+    this.DoctorService.getDoctors().subscribe((data:any)=>{
+      this.doctors= data;
+    }, err => this.errorMessage = err)
+  }
+
+  displayBySearch(spec:string, pro:string)
+  {
+    console.log("Specialty: " + spec);
+    console.log("Provider: " + pro);
+    this.DoctorService.getDoctorsBySpecialty(spec, pro).subscribe((data) =>
+    {
+      this.doctors = data;
+    }, err => this.errorMessage = err)
+  }
+
+
+}
---
+++ src/app/models/doctor.ts
@@ -0,0 +1,11 @@
+export class Doctor {
+
+    doctorId   !: number;
+    doctorName !: string;
+    phoneNumber !: string;
+    email     !: string;
+
+
+    //might need to change this
+    specialty !: string;
+}
