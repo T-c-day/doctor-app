0f7f110d7cce547a4f92534026cca832237ca401
Joel Rosas <joeli.rosas20@gmail.com>
2022-03-31 18:06:12 -0400

    Edited the sign up components

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Patient } from 'src/app/models/patient';
+import { PatientHandlerService } from 'src/app/services/patient-handler.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  patient !: Patient;
+  productForm !: FormGroup
+  errorMessage!:String;
+  successMessage! : string
+
+  constructor(public formBuilder: FormBuilder,public patientService: PatientHandlerService) { }
 
   ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      passwrord: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      address: ['', Validators.required]
+    })
+  }
+
+  signup(){
+    console.log("sign up")
+    this.patientService.savePatient(this.productForm.value).subscribe((data:any) =>{
+      this.successMessage = "Account successfully created "
+    }, err => this.errorMessage = err)
   }
 
 }
